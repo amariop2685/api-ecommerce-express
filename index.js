7921076ee8209571559d70054d3b6f8b721d1c3f
amariop2685 <acostap.marioa@gmail.com>
2024-02-24 21:16:41 -0300

    AGREGANDO LAS ACCIONES GET, POST, PUT, DELETE

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ app.get('/', (req, res) => {
   res.send('¡Hola, Express!');
 });
 
+app.get('/products', (req, res) => {
+  res.send('GET / Products');
+});
+
+app.post('/', (req, res) => {
+  res.send('POST / Products');
+});
+
+app.put('/', (req, res) => {
+  res.send('PUT / Products');
+});
+
+app.delete('/', (req, res) => {
+  res.send('DELETE / Products');
+});
 app.listen(puerto, () => {
   console.log('Servidor escuchando en http://localhost:' + puerto);
 });
